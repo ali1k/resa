ab4c042caffcdb153c16564a4c0840cb42448d0d
ali1k <hyperir@gmail.com>
2016-10-09 09:33:35 +0200

    added test

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
+var config = require('./config.js');
+var twitter = require('ntwitter');
 
-  
+var t = new twitter({
+    consumer_key: config.get['twitter_consumer_key'],
+    consumer_secret: config.get['twitter_consumer_secret'],
+    access_token_key: config.get['twitter_access_token_key'],
+    access_token_secret: config.get['twitter_access_token_secret']
+});
+t.stream('statuses/filter', { track: 'Iran' }, function(stream) {
+    stream.on('data', function (data) {
+        console.log(data.text);
+    });
+});
